13de965f3a946fa05e142cac0975e525234244ac
Usuario Local <luisecab@ucm.es>
2024-10-24 18:47:21 +0000

    ejercicio 2 Practica 4

--- ficheros_p3/ejercicio3/mostrar.c
+++ ficheros_p3/ejercicio3/mostrar.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+enum readType{BEGIN,END};
 
 int main(int argc, char *argv[])
 {
+	readType type = BEGIN;
+	int offset=0;
+	while((opt = getopt(argc,argv,"n:e"))!=-1){
+		
+		switch(opt)
+		{
+		case 'n':
+			offset = optarg;
+			break;
+		case 'e':
+			type = END;
+			break;
+		default:
+			exit(EXIT_FAILURE);
+		}
+	}
 
+	
 	return 0;
 }
