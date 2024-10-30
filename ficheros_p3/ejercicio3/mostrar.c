a983e29a9278ef94decdf538313de2e8b0025a5f
Usuario Local <luisecab@ucm.es>
2024-10-30 15:10:51 +0000

    Practica 2 Ejercicio 3

--- ficheros_p3/ejercicio3/mostrar.c
+++ ficheros_p3/ejercicio3/mostrar.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 enum readType{BEGIN,END};
 
 int main(int argc, char *argv[])
 {
-	readType type = BEGIN;
+	enum readType type = BEGIN;
 	int offset=0;
+	int opt;
 	while((opt = getopt(argc,argv,"n:e"))!=-1){
 		
 		switch(opt)
 		{
 		case 'n':
-			offset = optarg;
+			offset = atoi(optarg);
 			break;
 		case 'e':
 			type = END;
@@ -23,6 +26,22 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	char* filename = argv[optind];
+
+	int ind = open(filename,O_RDONLY);
+
+	if(type == BEGIN){
 	
+		lseek(ind,offset,SEEK_SET);
+	
+	}else{
+		lseek(ind,-offset,SEEK_END);
+	}
+
+	FILE* stream = fdopen(ind,"r");
+
+	while(feof(stream)==0){
+		printf("%c",fgetc(stream));
+	}
 	return 0;
 }
